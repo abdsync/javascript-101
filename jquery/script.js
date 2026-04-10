ae30128311710ed1e45e20837360e949b3d5b8da
abdsync <muhammed.abdulah21@gmail.com>
2026-04-10 19:56:12 +0500

    Added the jquery folder

---
+++ jquery/script.js
@@ -0,0 +1,20 @@
+// $("h1").hide();
+// $("h1#heading1").hide();
+// $("p.para2").hide();
+
+// $("p span").css("color", "orange"); // Here span is the tag within a tag. 
+
+// $("ul li:even").css("color", "maroon"); // Counting starts form zero. 
+// $("ul li:even").css("font-size", "40px");
+// $("ul li:odd").css("font-size", "30px");
+// $("ul#list li:odd").css("font-size", "30px"); // More specific 
+// $("ul li:nth-child(4)").css("list-style-type", "none");
+
+// $(":submit").hide();
+// $(":button").hide();
+// $(":text").hide();
+
+$("[href").css("color", "maroon");
+$("a[href='https://jquery.com/']").css("color", "green");
+
+
